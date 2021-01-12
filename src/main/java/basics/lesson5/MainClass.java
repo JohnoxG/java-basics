26e2f1aa304543c971a41791a291f0d176579642
Mariusz Czarny <mariusz.czarny88@gmail.com>
2021-01-12 20:24:21 +0100

    Lesson 5: statics vs objects

---
+++ src/main/java/basics/lesson5/MainClass.java
@@ -0,0 +1,9 @@
+package basics.lesson5;
+
+public class MainClass {
+	static void main(String[] args) {
+		int sum = StructuralProgramming.add(10, 20);
+		System.out.println(sum);
+		StructuralProgramming.showMessage("test message");
+	}
+}
---
+++ src/main/java/basics/lesson5/StructuralProgramming.java
@@ -0,0 +1,14 @@
+package basics.lesson5;
+
+public class StructuralProgramming {
+	static String name;
+	
+	static void showMessage(String message) {
+		add(1, 2);
+		System.out.println(message+ " " + name);
+	}
+
+	static int add(int valueOne, int valueTwo) {
+		return valueOne + valueTwo;
+	}
+}
