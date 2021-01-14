f136057e9215a75c94a8c02cea581bbe4adc0dbb
Mariusz Czarny <mariusz.czarny88@gmail.com>
2021-01-14 19:45:09 +0100

    Lesson 11: Operators

---
+++ src/main/java/basics/lesson11/Operators.java
@@ -0,0 +1,39 @@
+package basics.lesson11;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		String testVariable = "some text";
+		System.out.println(checkValue(testVariable));
+		int result = (testVariable.equals("some text")) ? 0 : 1;
+		System.out.println(result);
+
+		int sum=1;
+		
+		sum+=1;
+		int myIntFromDouble = (int) 10.2;
+		
+		int x = 1;
+
+		if (x == 0 && printInfo()) {
+			System.out.println("Operator logiczny zadzia³a³");
+		}
+
+		if (x == 0 & printInfo()) {
+			System.out.println("Operator bitowy zadzia³a³");
+		}
+	}
+
+	private static boolean printInfo() {
+		System.out.println("Metoda siê wykona³a");
+		return true;
+	}
+
+	private static int checkValue(String testVariable) {
+		if (testVariable.equals("some text")) {
+			return 0;
+		} else {
+			return 1;
+		}
+	}
+}
