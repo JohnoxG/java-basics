96aadde6f9351dcd133bc169ddda3ee588ca3548
Mariusz Czarny <mariusz.czarny88@gmail.com>
2021-01-16 19:51:01 +0100

    Lesson 14: enum

---
+++ src/main/java/basics/lesson14/ConstantsEnum.java
@@ -0,0 +1,25 @@
+package basics.lesson14;
+
+public class ConstantsEnum {
+	public enum MyEnum {
+		OK("ok");
+		private final String name;
+		
+		private MyEnum(String name) {
+			this.name= name;
+		}
+		
+		public String getName() {
+			return name;
+		}
+	}
+	
+	public static void main(String [] args) {
+		System.out.println(MyEnum.OK.getName());
+		System.out.println(MyEnum.OK);
+		MyEnum[] values = MyEnum.values();
+		for (MyEnum myEnum : values) {
+			System.out.println(myEnum.getName());
+		}
+	}
+}
